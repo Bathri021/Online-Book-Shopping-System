99c91273b8201c7ecbeae9902a0e8faf64e6be72
Bathri021 <58761277+Bathri021@users.noreply.github.com>
2020-02-06 22:28:22 +0530

    Update HomePage.aspx.cs

--- HomePage.aspx.cs
+++ HomePage.aspx.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+
 
 namespace Online_Book_Shopping_System
 {
