cae34c887d2dbef0fa36fb1a0a0a02f20ba0815c
Bathri021 <58761277+Bathri021@users.noreply.github.com>
2020-02-06 22:27:46 +0530

    Update MasterPage.Master.cs

--- MasterPage.Master.cs
+++ MasterPage.Master.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace Online_Book_Shopping_System
 {
